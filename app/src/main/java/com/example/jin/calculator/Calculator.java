a8f0526fe3e9535118c231593b715f53c218d09e
LXJIN <751137697@qq.com>
2017-05-10 22:40:13 +0800

    20170510

--- app/src/main/java/com/example/jin/calculator/Calculator.java
+++ app/src/main/java/com/example/jin/calculator/Calculator.java
@@ -6,8 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import android.widget.Toast;
 
 
 public class Calculator extends AppCompatActivity {
@@ -16,8 +15,9 @@ public class Calculator extends AppCompatActivity {
     private  double secondNumber = 0;// 第二次输入的值
     private String NumS = null;//数字字符
     private static String result = null;// 显示的结果
-    private static String Old_result = null;// 中间的结果
-    private  int op=0;//判断操作数
+    private static String Old_result = null;// 中间的结果，形式：1+
+    private  int op=0;//判断操作符
+    private static int Num_OP = 0;//操作符的个数
     private double num = 0;//计算结果数值
 
     private int[] btnNum_id = {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5,
@@ -56,55 +56,43 @@ public class Calculator extends AppCompatActivity {
 
         btnBackspace.setOnClickListener(new Button.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {//退格
                 result = tvResult.getText().toString();
                 if(isEmpty(result)) return;
-                else
-                    tvResult.setText(result.substring(0, result.length()-1));//截取字符串
+                else {
+                    /*NumS = result.substring(result.length() - 1, result.length());//截取最后一个字符
+                    while (NumS.equals("+") || NumS.equals("-") || NumS.equals("X")
+                            || NumS.equals("÷")){
+                        op=0;
+                        Num_OP--;
+                    }*/
+                    result = result.substring(0, result.length() - 1);//截取字符串
+                    tvResult.setText(result);
+
+                }
             }
         });
 
         btnCE.setOnClickListener(new Button.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                result = null;
-                firstNumber = secondNumber = 0;
+            public void onClick(View v) {//清空
+                result = Old_result = NumS = null;
+                num = firstNumber = secondNumber = 0;
+                op = 0;
+                Num_OP = 0;
                 tvResult.setText(result);
             }
         });
 
         btnEq.setOnClickListener(new Button.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                try{
-                    NumS = result.replace(Old_result, "");
-                    System.out.println("NumS:" + NumS);
-                    secondNumber=Double.valueOf(NumS);
-                }catch(NumberFormatException e) {
-                    System.out.println(e);
-                }
-                switch (op) {
-                    case 1:
-                        num = firstNumber + secondNumber;
-                        break;
-                    case 2:
-                        num = firstNumber - secondNumber;
-                        break;
-                    case 3:
-                        num = firstNumber * secondNumber;
-                        break;
-                    case 4:
-                        num = firstNumber / secondNumber;
-                        break;
-                    default:
-                        num = 0;
-                        break;
-                }
+            public void onClick(View v) {//等于
+                result = tvResult.getText().toString();
+                compute();
                 //try{
-                System.out.println("firstNumber:" + firstNumber);
-                System.out.println("secondNumber:" + secondNumber);
-                System.out.println("num:" + num);
                 tvResult.setText(String.valueOf(num));
+                op = 0;
+                Num_OP--;
                // }catch(Resources.NotFoundException e){
                 //    System.out.println(e);
                // }
@@ -112,6 +100,7 @@ public class Calculator extends AppCompatActivity {
         });
     }
 
+    //输出内容到TextView上，可给firstnum赋值
     class tvShow implements Button.OnClickListener{
         @Override
         public void onClick(View v) {
@@ -120,43 +109,147 @@ public class Calculator extends AppCompatActivity {
             if (result.equals("00")) {
                 tvResult.setText(null);
             }else {
-                try{
-                    //Pattern p = Pattern.compile("[^0-9]");
+                //System.out.println("Start_NumOP:" + Num_OP);
+                //操作符个数为1时，判断第二个运算符是否输入
+                if(Num_OP == 1){
+                    result = result + input;
+                    tvResult.setText(result);
                     switch (v.getId()) {
                         case R.id.btnAdd:
-                            //firstNumS = p.matcher(result).replaceAll("").trim();
-                            firstNumber=Double.valueOf(result);
-                            op=1;
-                            Old_result = result + input;
+                            Num_OP++;//操作符个数加1
                             break;
                         case R.id.btnSub:
-                            //firstNumS = p.matcher(result).replaceAll("").trim();
-                            firstNumber=Double.valueOf(result);
-                            op=2;
-                            Old_result = result + input;
+                            Num_OP++;
                             break;
                         case R.id.btnMul:
-                            //firstNumS = p.matcher(result).replaceAll("").trim();
-                            firstNumber=Double.valueOf(result);
-                            op=3;
-                            Old_result = result + input;
+                            Num_OP++;
                             break;
                         case R.id.btnDiv:
-                            //firstNumS = p.matcher(result).replaceAll("").trim();
-                            firstNumber=Double.valueOf(result);
-                            op=4;
-                            Old_result = result + input;
+                            Num_OP++;
+                            break;
+                        default:
                             break;
                     }
-                    result = result +input;
+                    //System.out.println("result:" + result);
+                    //System.out.println("Old_result:" + Old_result);
+                    //System.out.println("NumOP:" + Num_OP);
+                }
+                /*操作符个数为2时，判断第二个运算符，将第一个运算符的计算结果赋值给第一个操作数
+                firstnum*/
+                if(Num_OP == 2) {
+                    result = result.substring(0, result.length()-1);//删除最后一个字符
+                    //System.out.println("result:" + result);
+                    //System.out.println("Old_result:" + Old_result);
+                    compute();//计算值
+                    Num_OP--;//操作符个数减一
+                    result = String.valueOf(num) + input;
                     tvResult.setText(result);
-                    System.out.println("result:" + result);
-                    System.out.println("Old_result:" + Old_result);
-                }catch(NumberFormatException e){
-                    System.out.println(e);
+                    switch (v.getId()) {
+                        case R.id.btnAdd:
+                            firstNumber = num;
+                            op = 1;
+                            Old_result = result;
+                            break;
+                        case R.id.btnSub:
+                            firstNumber = num;
+                            op = 2;
+                            Old_result = result;
+                            break;
+                        case R.id.btnMul:
+                            firstNumber = num;
+                            op = 3;
+                            Old_result = result;
+                            break;
+                        case R.id.btnDiv:
+                            firstNumber = num;
+                            op = 4;
+                            Old_result = result;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                //操作符个数为0时，如有运算符，则给第一个操作数firstnum赋值
+                if(Num_OP == 0) {
+                    try {
+                        //Pattern p = Pattern.compile("[^0-9]");
+                        //Old_result = result.replace(input, "");//替换字符串
+                        switch (v.getId()) {
+                            case R.id.btnAdd:
+                                firstNumber = Double.valueOf(result);
+                                op = 1;
+                                Old_result = result + input;//保存中间结果
+                                Num_OP++;//操作符个数加1
+                                break;
+                            case R.id.btnSub:
+                                firstNumber = Double.valueOf(result);
+                                op = 2;
+                                Old_result = result + input;
+                                Num_OP++;
+                                break;
+                            case R.id.btnMul:
+                                firstNumber = Double.valueOf(result);
+                                op = 3;
+                                Old_result = result + input;
+                                Num_OP++;
+                                break;
+                            case R.id.btnDiv:
+                                firstNumber = Double.valueOf(result);
+                                op = 4;
+                                Old_result = result + input;
+                                Num_OP++;
+                                break;
+                            default:
+                                break;
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println(e);
+                    }
+                    result = result + input;
+                    tvResult.setText(result);
                 }
             }
+            //System.out.println("End_NumOP:" + Num_OP);
+            //System.out.println("result:" + result);
+            //System.out.println("Old_result:" + Old_result);
         }
+    }
+
+    //给第二个操作数secondnum赋值，计算结果num
+    private void compute(){
+        try{
+            NumS = result.replace(Old_result, "");//替换字符串
+            //System.out.println("NumS:" + NumS);
+            secondNumber=Double.valueOf(NumS);
+        }catch(NumberFormatException e) {
+            System.out.println(e);
+        }
+        switch (op) {
+            case 1:
+                num = firstNumber + secondNumber;
+                break;
+            case 2:
+                num = firstNumber - secondNumber;
+                break;
+            case 3:
+                num = firstNumber * secondNumber;
+                break;
+            case 4:
+                if(secondNumber != 0){
+                    num = firstNumber / secondNumber;
+                }else{
+                    Toast.makeText(getApplicationContext(), "0不能为除数",
+                            Toast.LENGTH_SHORT).show();
+                    num = firstNumber;
+                }
+                break;
+            default:
+                num = 0;
+                break;
+        }
+        //System.out.println("firstNumber:" + firstNumber);
+        //System.out.println("secondNumber:" + secondNumber);
+        //System.out.println("num:" + num);
     }
 
     //判断字符串是否为空
